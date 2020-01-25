188c95c8c618af49f18ae2146afa27921a449227
Sergi Llamas <shigeru.yamasaki.17@gmail.com>
2020-01-25 19:18:12 +0100

    Create list fragment layout

--- app/src/main/java/android/sllamas/speedrunrecords/list/RecordsListFragment.kt
+++ app/src/main/java/android/sllamas/speedrunrecords/list/RecordsListFragment.kt
@@ -1,5 +1,18 @@
 package android.sllamas.speedrunrecords.list
 
+import android.os.Bundle
+import android.sllamas.speedrunrecords.R
+import android.sllamas.speedrunrecords.common.inflate
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class RecordsListFragment : Fragment()
+class RecordsListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = container?.inflate(R.layout.fragment_records_list)
+}
