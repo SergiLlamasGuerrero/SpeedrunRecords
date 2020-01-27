ea96876e4fe63b9a3c8273efbd3cf7248551845f
Sergi Llamas <shigeru.yamasaki.17@gmail.com>
2020-01-27 20:28:16 +0100

    Craete run detail presenter

--- app/src/main/java/android/sllamas/speedrunrecords/ui/detail/RunDetailFragment.kt
+++ app/src/main/java/android/sllamas/speedrunrecords/ui/detail/RunDetailFragment.kt
@@ -8,11 +8,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class RunDetailFragment: Fragment() {
+class RunDetailFragment: Fragment(), RunDetailPresenter.View {
+
+    private val presenter by lazy { RunDetailPresenter() }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = container?.inflate(R.layout.fragment_run_detail)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        presenter.attachView(this)
+    }
 }
---
+++ app/src/main/java/android/sllamas/speedrunrecords/ui/detail/RunDetailPresenter.kt
@@ -0,0 +1,12 @@
+package android.sllamas.speedrunrecords.ui.detail
+
+import android.sllamas.speedrunrecords.ui.common.Presenter
+
+class RunDetailPresenter : Presenter<RunDetailPresenter.View>() {
+
+    override fun onViewAttached() {
+
+    }
+
+    interface View : Presenter.View
+}
