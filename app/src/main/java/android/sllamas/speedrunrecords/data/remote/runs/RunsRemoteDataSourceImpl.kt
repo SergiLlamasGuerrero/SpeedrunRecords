5863f2d7705eef0fa7c31c84ae7257559b3f1eeb
Sergi Llamas <shigeru.yamasaki.17@gmail.com>
2020-01-28 22:38:23 +0100

    Get service by constructor in datasources

--- app/src/main/java/android/sllamas/speedrunrecords/data/remote/runs/RunsRemoteDataSourceImpl.kt
+++ app/src/main/java/android/sllamas/speedrunrecords/data/remote/runs/RunsRemoteDataSourceImpl.kt
@@ -3,10 +3,13 @@ package android.sllamas.speedrunrecords.data.remote.runs
 import android.sllamas.data.source.RunsRemoteDataSource
 import android.sllamas.domain.Run
 import android.sllamas.speedrunrecords.data.remote.WebServiceProvider
+import android.sllamas.speedrunrecords.data.remote.games.GamesApi
 import io.reactivex.Single
 
-class RunsRemoteDataSourceImpl : RunsRemoteDataSource {
+class RunsRemoteDataSourceImpl(
+    private val service: RunsApi = WebServiceProvider.getService(RunsApi::class.java)
+) : RunsRemoteDataSource {
 
     override fun getRuns(gameId: String): Single<List<Run>> =
-        WebServiceProvider.getService(RunsApi::class.java).getRuns(gameId).map { wrapper -> wrapper.data.map { it.transformToDomain() } }
+        service.getRuns(gameId).map { wrapper -> wrapper.data.map { it.transformToDomain() } }
 }
